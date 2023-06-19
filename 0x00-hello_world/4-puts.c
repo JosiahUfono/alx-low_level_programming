a181909c212cf546c233f3eb3cb00e192b7493e6
Josiah <josiahufono@gmail.com>
2023-06-19 14:46:59 -0700

    a C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
