c6f4517fe120cd24331e3b44c0b95c6eef9c9937
Josiah <josiahufono@gmail.com>
2023-06-23 20:57:38 -0700

    a function that checks for a digit (0 through 9).
    Prototype: int _isdigit(int c);
    Returns 1 if c is a digit
    Returns 0 otherwisewq

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if input is a digit
+ * Return: 1 if uppercase, 0 otherwise
+ * @c: the digit to be checked
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
