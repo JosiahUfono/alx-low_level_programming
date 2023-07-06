1fc638a2bc42af501b2ce4303f65d24fcd1185f9
Josiah <josiahufono@gmail.com>
2023-07-06 06:59:39 -0700

    Task 1 and 2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,14 +3,14 @@
 /**
  * _print_rev_recursion - This function prints a string in reverse order.
  *
- * @s - The string to be printed
+ * @s - s is the string to be printed
  */
 
 void _print_rev_recursion(char *s)
 {
-   if (*str)
-   {
-       _print_rev_recursion(s+1);
-       _putchar(*s);
-   }
+	if (*s)
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
 }
